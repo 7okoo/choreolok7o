8a90ed8f79cf05fd9e54e91dedfd0b4016cd12e9
7okoo <124653941+7okoo@users.noreply.github.com>
2023-04-30 17:52:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "-----  Starting argo...----- "
-Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiZTJmMjM3MmYtNDE4Yy00YzU2LWFhY2QtMDI4NWViOTRiYTc4IiwicyI6Ik5tUTBOMlUyWkRjdFpUWTVaaTAwTjJOaExUZ3lNemN0TkRaak1HTmtORGszTkRJMSJ9'}
+Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiOTY4N2MxNzctYjc4My00Njc5LThkOTMtZmM4ZGUwZGMxMjIyIiwicyI6IllUTmtNV0l3TVdFdE1XWTNaUzAwTWpneExUazBNR0l0T1dNMk5UUmxaRFkyWVRjeSJ9'}
 
 chmod +x ./cloudf.sh
 ./cloudf.sh tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
